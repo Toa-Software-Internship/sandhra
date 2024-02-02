e9e4a2fb89811c8ca01dc4bd1c457e8d2bde5f3e
Sandhra <sandhrasonabijoy@gmail.com>
2024-02-02 14:01:08 +0900

    final button created using css similar to design

--- frontend/frontend/src/atoms/button/Button.jsx
+++ frontend/frontend/src/atoms/button/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./Button.css"
 
 const Button = () => {
   return (
     <div>
-      <button>SUBMIT</button>
+      <button className='button-style'>SUBMIT</button>
     </div>
   )
 }
