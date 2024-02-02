05ac795bb9990ff3ca209b8c0b6f19b0c987a390
Sandhra <sandhrasonabijoy@gmail.com>
2024-02-02 13:08:21 +0900

    Front end begins, button creation implemented

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './atoms/button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+
+
+    </div>
+  );
+}
+
+export default App;
