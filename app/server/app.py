19fdceae3029ddec2a1655f2b7845c1cfd69f3df
Sandhra <sandhrasonabijoy@gmail.com>
2024-02-06 11:29:17 +0900

    Installation of axios , addition of access control for URLs in backend ,get statement for Lots executed

--- app/server/app.py
+++ app/server/app.py
@@ -2,12 +2,25 @@ from fastapi import FastAPI
 from server.routes.lot import router as LotRouter
 from server.routes.data import router as DataRouter
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 #Lot Router and Data Router
 
 app.include_router(DataRouter,tags=["Data"],prefix="/data")
 app.include_router(LotRouter,tags=["Lot"],prefix="/lot")
 
+origins = [
+"http://localhost:3000",
+"http://localhost:8000",
+]
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],  
+allow_headers=["*"],  
+)
 
 @app.get("/", tags=["Root"])
 async def read_root():
--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
 import Heading from './atoms/Heading/Heading';
 import SmallHead from './atoms/SmallHead/SmallHead';
 import Button from './atoms/button/Button';
-import Descr from './atoms/descr/Descr';
 import Dropdown from './atoms/dropdown/Dropdown';
 import Image from './atoms/Images/Image';
 import Navigation from './atoms/Navigation/Navigation';
@@ -10,6 +11,22 @@ import BasicTable from './atoms/Table/Table';
 import BottomNavigation from './atoms/BottomNavigation/BottomNavigation';
 
 function App() {
+  const [lotOptions,setLotOptions] = useState([]);
+
+  useEffect(()=>{
+    axios
+    .get("http://localhost:8000/lot/")
+    .then((response)=>{
+      console.log(response.data.data);
+      setLotOptions(response.data.data)
+    })
+  })
+
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log("ButtonClicked");
+  }
   return (
     <div className="main">
 
@@ -20,7 +37,7 @@ function App() {
       <div className='bar'>
       
         <Dropdown/>
-        <Button className="submit-button" text="SUBMIT"/>
+        <Button className="submit-button" text="SUBMIT" handleChange={handleSubmit}/>
 
       </div>
       <div className='table-size'>
