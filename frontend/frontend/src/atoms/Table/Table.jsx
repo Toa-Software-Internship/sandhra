0b2e759a98f20c40e24915d0e5c51003b022be05
Sandhra <sandhrasonabijoy@gmail.com>
2024-02-07 14:02:39 +0900

    Project completed with modifications to the date format as well as Capitalizing the first letter of Work Type

--- frontend/frontend/src/atoms/Table/Table.jsx
+++ frontend/frontend/src/atoms/Table/Table.jsx
@@ -8,7 +8,19 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+// Function to format the date to yy-mm-dd
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear().toString(); 
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2); 
+  return `${year}-${month}-${day}`;
+}
 
+// Function to capitalize the first letter of a string
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function BasicTable({data}) {
   return (
@@ -18,15 +30,15 @@ export default function BasicTable({data}) {
           <TableRow >
             <TableCell className='heading' sx={{color:"rgb(42, 71, 4)" , fontWeight:"bold", fontSize:"30px"} }>DATE </TableCell>
             <TableCell className='heading' sx={{color:"rgb(42, 71, 4)" , fontWeight:"bold", fontSize:"30px"} }>YIELD RECEIVED(gms)</TableCell>
-            <TableCell className='heading'sx={{color:"rgb(42, 71, 4)" , fontWeight:"bold", fontSize:"30px"} }>WORKTYPE&nbsp;</TableCell>
+            <TableCell className='heading'sx={{color:"rgb(42, 71, 4)" , fontWeight:"bold", fontSize:"30px"} }>WORK TYPE&nbsp;</TableCell>
           </TableRow>
         </TableHead>
         <TableBody  className='heading' sx={{ fontWeight:"bold"} } >
           {data.map((item) => (
             <TableRow>
-              <TableCell  className='heading'  sx={{ fontWeight:"bold",fontSize:"20px"} }>{item.date}\</TableCell>
+              <TableCell  className='heading'  sx={{ fontWeight:"bold",fontSize:"20px"} }>{formatDate(item.date)}</TableCell>
               <TableCell  className='heading'  sx={{ fontWeight:"bold", fontSize:"20px"} }>{item.yield_obtained}</TableCell>
-              <TableCell  className='heading'  sx={{ fontWeight:"bold",fontSize:"20px"} }>{item.workdone}</TableCell>
+              <TableCell  className='heading'  sx={{ fontWeight:"bold",fontSize:"20px"} }>{capitalizeFirstLetter(item.workdone)}</TableCell>
             </TableRow>
             
           ))}
